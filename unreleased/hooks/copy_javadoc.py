21282505d7c900c2d6f60c3c781b89871a36d238
Luis Emidio <luisfuturist@gmail.com>
2026-07-08 00:32:13 -0300

    Deployed 8116b10 to unreleased with MkDocs 1.6.1 and mike 2.2.0

--- unreleased/hooks/copy_javadoc.py
+++ unreleased/hooks/copy_javadoc.py
@@ -2,15 +2,24 @@ import os
 import shutil
 
 def on_post_build(config):
-    # Javadoc is built in modules/clock-time-common/build/docs/javadoc
-    # We want to copy it to site_dir/apidocs
-    source_folder = 'modules/clock-time-common/build/docs/javadoc'
+    potential_folders = [
+        'modules/clock-time-common/build/docs/javadoc',
+        'modules/clock-time-paper/build/docs/javadoc',
+        'build/docs/javadoc'
+    ]
+    
+    source_folder = None
+    for folder in potential_folders:
+        if os.path.exists(folder):
+            source_folder = folder
+            break
+            
     target_folder = os.path.join(config['site_dir'], 'apidocs')
     
-    if os.path.exists(source_folder):
+    if source_folder:
         if os.path.exists(target_folder):
             shutil.rmtree(target_folder)
         shutil.copytree(source_folder, target_folder)
         print(f"Copied Javadoc from {source_folder} to {target_folder}")
     else:
-        print(f"Warning: Javadoc folder not found at {source_folder}")
+        print("Warning: No Javadoc folder found.")
