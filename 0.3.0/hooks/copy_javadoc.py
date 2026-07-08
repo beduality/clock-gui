f13ca9cf22a234e0ddef00ef8fec2e03886efb18
Luis Emidio <luisfuturist@gmail.com>
2026-07-08 00:21:25 -0300

    Deployed 8116b10 to 0.3.0 with MkDocs 1.6.1 and mike 2.2.0

---
+++ 0.3.0/hooks/copy_javadoc.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+def on_post_build(config):
+    # Search for Javadocs in any of the potential locations across different versions
+    potential_folders = [
+        'modules/clock-time-common/build/docs/javadoc',
+        'modules/clock-time-paper/build/docs/javadoc',
+        'build/docs/javadoc'
+    ]
+    
+    source_folder = None
+    for folder in potential_folders:
+        if os.path.exists(folder):
+            source_folder = folder
+            break
+            
+    target_folder = os.path.join(config['site_dir'], 'apidocs')
+    
+    if source_folder:
+        if os.path.exists(target_folder):
+            shutil.rmtree(target_folder)
+        shutil.copytree(source_folder, target_folder)
+        print(f"Copied Javadoc from {source_folder} to {target_folder}")
+    else:
+        print("Warning: No Javadoc folder found.")
